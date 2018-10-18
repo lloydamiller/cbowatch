770e7c354c728454e5c656e8587f84bd1a69ce54
lloydamiller <miller2382@protonmail.com>
2018-10-18 13:47:12 -0400

    1.0 Bot

--- run.py
+++ run.py
@@ -1,42 +1,56 @@
 # Core Python Script For Cycling Through
 
-import tweepy
-import apikeys as keys
+from datetime import datetime
+import time
 from rssfeed import get_new_feed_entries
 from tweettext import make_tweet_text
-
-
-def push_tweet(tweet_text):
-    auth = tweepy.OAuthHandler(keys.API_KEY, keys.API_KEY_SECRET)
-    auth.set_access_token(keys.ACCESS_TOKEN, keys.ACCESS_TOKEN_SECRET)
-    api = tweepy.API(auth)
-    try:
-        api.update_status(tweet_text)
-    except ConnectionError:
-        print("OH NO, ERROR POSTING TWEET: %s" % tweet_text)
-
-    return
-
+from tweet import push_tweet
 
 if __name__ == '__main__':
 
+    last_run = False  # resets after first successful run.
     last_item = ''
 
-    # open RSS feed check for new items
-    new_entries = get_new_feed_entries(last_item)
+    # initialize logger for Cycle module
+    while True:
+        current_time = datetime.now()
 
-    # for each new item, process for Tweet Text
-    tweets = []
-    for new_entry in new_entries:
-        tweet = make_tweet_text(new_entry['title'], new_entry['link'])
-        tweets.append(tweet)
+        if not last_run:
+            run_condition = True
 
-    # post new Tweet to Twitter
-    for tweet in tweets:
-        # push_tweet(tweet)
-        print(tweet)
+        else:
+            # True if the program has not has a successful cycle during the current hour
+            has_not_run_this_hour = last_run.hour != current_time.hour and last_run.day != current_time.day
 
-    if len(new_entries) > 0:
-        last_item = new_entries[0]['link']
+            # True if it is every fourth hour (0, 4, 8, 12, 16, 20)
+            every_fourth_hour = current_time.hour % 2 == 0
+
+            run_condition = every_fourth_hour and has_not_run_this_hour
+
+        if not run_condition:
+            print("[*] Not Ready To Run")
+            time.sleep(360)
+
+        else:
+            # open RSS feed check for new items
+            new_entries = get_new_feed_entries(last_item)
+
+            # for each new item, process for Tweet Text
+            tweets = []
+            for new_entry in new_entries:
+                tweet = make_tweet_text(new_entry['title'], new_entry['link'])
+                tweets.append(tweet)
+
+            # post new Tweet to Twitter
+
+            if len(new_entries) > 0:
+                print("[*] Publishing %i new tweets" % len(tweets))
+                for tweet in tweets:
+                    push_tweet(tweet)
+                last_item = new_entries[0]['link']
+            else:
+                print("[*] No new entries to update at this time.")
+            last_run = datetime.now()
+            time.sleep(60)
 
     pass
---
+++ tweet.py
@@ -0,0 +1,18 @@
+"""
+ Takes a tweet, opens up the API connection and pushes the new tweet
+"""
+
+import tweepy
+import apikeys as keys
+
+
+def push_tweet(tweet_text):
+    auth = tweepy.OAuthHandler(keys.API_KEY, keys.API_KEY_SECRET)
+    auth.set_access_token(keys.ACCESS_TOKEN, keys.ACCESS_TOKEN_SECRET)
+    api = tweepy.API(auth)
+    try:
+        api.update_status(tweet_text)
+    except ConnectionError:
+        print("[!] OH NO, ERROR POSTING TWEET: %s" % tweet_text)
+
+    return
